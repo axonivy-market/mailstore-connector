b9c42fcb5921e420fe87b99aaf9b6e0122909fd4
Peter Stieglecker <peter.stieglecker@axonivy.com>
2022-10-25 15:03:15 +0200

    initial version of mail parts handling

--- mailstore-connector-demo/src/com/axonivy/market/mailstore/connector/demo/DemoService.java
+++ mailstore-connector-demo/src/com/axonivy/market/mailstore/connector/demo/DemoService.java
@@ -1,13 +1,16 @@
 package com.axonivy.market.mailstore.connector.demo;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Part;
+import javax.mail.Session;
 
 import com.axonivy.market.mailstore.connector.MailStoreService;
 import com.axonivy.market.mailstore.connector.MailStoreService.MessageIterator;
+import com.axonivy.market.mailstore.connector.MessageService;
 
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.log.Logger;
@@ -17,23 +20,48 @@ public class DemoService {
 
 	public static void handleMessage(Message message) throws MessagingException, IOException {
 		LOG.info("Working on message {0} received at {1}", message.getSubject(), message.getReceivedDate());
-		LOG.info("Other: {0}", message.getContent());
+		LOG.info("Class: {0}", message.getContent().getClass());
 
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		message.writeTo(bos);
+		Collection<Part> parts = MessageService.allParts(message, null);
+		for (Part part : parts) {
+			LOG.info("Part: {0} {1}", part.getContentType(), part.getContent().getClass());
+		}
 
-		LOG.info("Content:\n{0}", bos.toString());
+		//		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		//		message.writeTo(bos);
 
+		//		LOG.info("Content:\n{0}", bos.toString());
 	}
 
 	public static void main(String[] args) throws MessagingException, IOException {
-		MessageIterator iterator = MailStoreService.mailIterator("localhost-imap", "INBOX", null, false, null);
+		MessageIterator iterator = MailStoreService.messageIterator("localhost-imap", "INBOX", null, false, null);
 
 		while (iterator.hasNext()) {
 			Message message = iterator.next();
+			showMessage(message);
+			iterator.handledMessage();
+		}
+	}
+
+	private static void showMessage(Message message) throws MessagingException, IOException {
+
+		try(ClassLoaderContext ccl = new ClassLoaderContext(Session.class)) {
 			LOG.info("Got message: {0}", message.getSubject());
 			LOG.info("Content: {0}", message.getContent());
-			iterator.handledMessage();
+		}
+	}
+
+	private static class ClassLoaderContext implements AutoCloseable {
+
+		private ClassLoader originalClassLoader;
+		public ClassLoaderContext(Class<?> clazz) {
+			originalClassLoader = Thread.currentThread().getContextClassLoader();
+			Thread.currentThread().setContextClassLoader(clazz.getClassLoader());
+		}
+
+		@Override
+		public void close() {
+			Thread.currentThread().setContextClassLoader(originalClassLoader);
 		}
 	}
 }
---
+++ mailstore-connector/src/com/axonivy/market/mailstore/connector/MessageService.java
@@ -0,0 +1,79 @@
+package com.axonivy.market.mailstore.connector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Part;
+import javax.mail.internet.MimeMultipart;
+
+import ch.ivyteam.ivy.bpm.error.BpmError;
+import ch.ivyteam.ivy.bpm.error.BpmPublicErrorBuilder;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.log.Logger;
+
+public class MessageService {
+	private static final MessageService INSTANCE = new MessageService();
+	private static final Logger LOG = Ivy.log();
+	private static final String ERROR_BASE = "mailstore:connector:message";
+
+	public static MessageService get() {
+		return INSTANCE;
+	}
+
+	public static Collection<Part> allParts(Message message, Predicate<Part> predicate) {
+		List<Part> parts = new ArrayList<>();
+		try {
+			collectParts(parts, message, 1, predicate);
+		}
+		catch(Exception e) {
+			throw buildError("iterator").withCause(e).build();
+		}
+
+		return parts;
+	}
+
+	private static void collectParts(List<Part> parts, Part part, int level, Predicate<Part> predicate) throws MessagingException, IOException {
+		parts.add(part);
+		if(part.isMimeType("multipart/*")) {
+			MimeMultipart multipart = (MimeMultipart) part.getContent();
+			int count = multipart.getCount();
+			for (int i = 0; i < count; i++) {
+				BodyPart bodyPart = multipart.getBodyPart(i);
+				if(predicate == null || predicate.test(bodyPart)) {
+					collectParts(parts, bodyPart, level+1, predicate);
+				}
+			}
+		}
+		else if(part.isMimeType("message/*")) {
+			Message message = (Message) part.getContent();
+			collectParts(parts, message, level+1, predicate);
+		}
+	}
+
+	public static Predicate<Part> mimeType(String mimeType) {
+		return p -> {
+			try {
+				return p.isMimeType(mimeType);
+			} catch (MessagingException e) {
+				throw buildError("predicate:mimetype").build();
+			}
+		};
+	}
+
+	public static Predicate<Part> noSubMessage() {
+		return Predicate.not(mimeType("message/*"));
+	}
+
+
+
+	private static BpmPublicErrorBuilder buildError(String code) {
+		BpmPublicErrorBuilder builder = BpmError.create(ERROR_BASE + ":" + code);
+		return builder;
+	}
+}
