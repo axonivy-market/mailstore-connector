ffba72577c368053425e8dba7eeca42f5dd52c3a
vinv <vi.nongvan@axonactive.com>
2024-10-25 11:14:38 +0700

    refractore: using rest inside java code instead of make function callable

--- mailstore-connector/src/com/axonivy/connector/oauth/AzureOauth2UserPasswordProvider.java
+++ mailstore-connector/src/com/axonivy/connector/oauth/AzureOauth2UserPasswordProvider.java
@@ -1,17 +1,14 @@
 package com.axonivy.connector.oauth;
 
-import java.util.Optional;
-
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.axonivy.connector.mailstore.MailStoreService;
 
-import ch.ivyteam.ivy.bpm.error.BpmError;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.process.call.SubProcessCall;
-import ch.ivyteam.ivy.process.call.SubProcessCallResult;
 import ch.ivyteam.log.Logger;
 
 public class AzureOauth2UserPasswordProvider implements UserPasswordProvider {
@@ -21,6 +18,8 @@ public class AzureOauth2UserPasswordProvider implements UserPasswordProvider {
 	private static final String SECRET_KEY = "secretKey";
 	private static final String GRANT_TYPE = "grantType";
 	private static final String SCOPE = "scope";
+	
+	private static final String REST_CLIENT = "getTokenAzureOAuth";
 	
 	@Override
 	public String authenticate(String storeName) {
@@ -39,45 +38,33 @@ public class AzureOauth2UserPasswordProvider implements UserPasswordProvider {
 		return form;
 	}
 	
-	private String getToken(String storeName) {
-		Form form = buildForm(storeName);
-
-		String tenantId = MailStoreService.getVar(storeName, TENANT_ID);
-		String tokenUrlPrefix = MailStoreService.getVar(storeName, "tokenUrl.tokenUrlPrefix");
-		String tokenUrlSuffix = String.format(MailStoreService.getVar(storeName, "tokenUrl.tokenUrlSuffix"), tenantId);
-
-		if (StringUtils.isBlank(tokenUrlPrefix) || StringUtils.isBlank(tokenUrlSuffix)) {
-			LOG.error("url to get token cannot be null or empty");
-			throw MailStoreService.buildError("getToken").withMessage("url to get token cannot be null or empty")
-					.build();
-		}
-
-		TokenDTO result = null;
-		BpmError error = null;
-		SubProcessCallResult callResult = SubProcessCall.withPath("OAuth2Feature").withStartName("getToken")
-				.withParam("form", form).withParam("tokenUrlPrefix", tokenUrlPrefix)
-				.withParam("tokenUrlSuffix", tokenUrlSuffix).call();
-
-		if (callResult != null) {
-			Optional<Object> o = Optional.ofNullable(callResult.get("token"));
-			if (o.isPresent()) {
-				result = (TokenDTO) o.get();
-			} else {
-				Optional<Object> e = Optional.ofNullable(callResult.get("error"));
-				if (e.isPresent()) {
-					error = (BpmError) e.get();
-					LOG.error(error);
-					throw error;
-				}
-			}
-		}
-
-		if (null == result || StringUtils.isBlank(result.getAccessToken())) {
-			LOG.error("access token cannot be null or empty");
-			throw MailStoreService.buildError("getToken").withMessage("access token cannot be null or empty").build();
-		}
-
-		return result.getAccessToken();
+	private Response sendTokenRequest(String tenantId, Form form) {
+	    return Ivy.rest().client(REST_CLIENT)
+	        .path(tenantId)
+	        .path("oauth2/v2.0/token")
+	        .request()
+	        .header("Content-Type", "application/x-www-form-urlencoded")
+	        .post(Entity.form(form));
 	}
 	
+	private String getToken(String storeName) {
+		Form form = buildForm(storeName);
+		String tenantId = MailStoreService.getVar(storeName, TENANT_ID);
+
+		Response response = sendTokenRequest(tenantId, form);
+
+		if (null == response) {
+			LOG.error("response cannot be null");
+			throw MailStoreService.buildError("getToken").withMessage("response cannot be null").build();
+		}
+
+		TokenDTO tokenDto = OAuthUtils.extractToken(response);
+
+		if (null == tokenDto || StringUtils.isEmpty(tokenDto.getAccessToken())) {
+			throw new IllegalStateException("Failed to read 'access_token' from " + response);
+		}
+
+		return tokenDto.getAccessToken();
+	}
+
 }
