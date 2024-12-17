b28f39e0979f26c95ba7e33aeea43cd6f2034af9
nqhoan-axonivy <hoan.nguyen01@axonactive.com>
2024-12-17 08:57:29 +0700

    Handle feedback

--- mailstore-connector/src/com/axonivy/connector/oauth/ssl/MailStoreTrustManager.java
+++ mailstore-connector/src/com/axonivy/connector/oauth/ssl/MailStoreTrustManager.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 import javax.net.ssl.X509TrustManager;
 
+import ch.ivyteam.ivy.environment.Ivy;
+
 public class MailStoreTrustManager implements X509TrustManager {
 	final X509TrustManager defaultTrustManager;
 	final X509TrustManager customTrustManager;
@@ -18,8 +20,12 @@ public class MailStoreTrustManager implements X509TrustManager {
 
 	private X509Certificate[] mergeCertificates() {
 		ArrayList<X509Certificate> resultingCerts = new ArrayList<>();
-		resultingCerts.addAll(Arrays.asList(defaultTrustManager.getAcceptedIssuers()));
-		resultingCerts.addAll(Arrays.asList(customTrustManager.getAcceptedIssuers()));
+		if (defaultTrustManager != null) {
+			resultingCerts.addAll(Arrays.asList(defaultTrustManager.getAcceptedIssuers()));
+		}
+		if (customTrustManager != null) {
+			resultingCerts.addAll(Arrays.asList(customTrustManager.getAcceptedIssuers()));
+		}
 		return resultingCerts.toArray(new X509Certificate[resultingCerts.size()]);
 	}
 
@@ -32,13 +38,19 @@ public class MailStoreTrustManager implements X509TrustManager {
 	public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 		try {
 			customTrustManager.checkServerTrusted(chain, authType);
-		} catch (CertificateException e) {
-			defaultTrustManager.checkServerTrusted(chain, authType);
+		} catch (Exception e) {
+			Ivy.log().error("Cannot read TrustedServer from TrustManager", e);
+			if (defaultTrustManager != null) {
+				defaultTrustManager.checkServerTrusted(chain, authType);
+			}
 		}
 	}
 
 	@Override
 	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		if (defaultTrustManager == null) {
+			return;
+		}
 		defaultTrustManager.checkClientTrusted(mergeCertificates(), authType);
 	}
 }
