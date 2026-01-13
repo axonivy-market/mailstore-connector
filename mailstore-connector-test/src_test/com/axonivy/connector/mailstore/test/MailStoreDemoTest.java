7849d374fdac8aa3f3d7c17faf640cd4aa022708
quanpham-axonivy <quan.phamvan@axonactive.com>
2026-01-13 10:29:31 +0700

    add host

--- mailstore-connector-test/src_test/com/axonivy/connector/mailstore/test/MailStoreDemoTest.java
+++ mailstore-connector-test/src_test/com/axonivy/connector/mailstore/test/MailStoreDemoTest.java
@@ -96,6 +96,7 @@ class MailStoreDemoTest {
 
 	private void configureDemo(AppFixture fixture, String userName) {
 		String imap = "mailstoreConnector.localhostImap";
+		fixture.var(imap + ".host", "mailserver");
 		fixture.var(imap + ".port", "993");
 		fixture.var(imap + ".user", userName);
 		fixture.var(imap + ".password", "password123");
